ec51a10b5db43e11984f6b5c0798379eeb382d9c
Tara Sankhalpara <tsankhalpara@mac.com>
2019-03-20 12:34:32 +0000

    edit file back to original code and creat new ruby file for each exercise

--- directory.rb
+++ directory.rb
@@ -32,15 +32,15 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each_with_index do |student, i|
-    puts "#{i+1}.#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-# students = input_students
+students = input_students
 print_header
 print(students)
 print_footer(students)
