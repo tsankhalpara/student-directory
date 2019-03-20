71ad6c54f1a3f58d7c8467dd393caa179e62aa2c
Tara Sankhalpara <tsankhalpara@mac.com>
2019-03-20 12:13:09 +0000

    add user input to manually add names

--- directory.rb
+++ directory.rb
@@ -12,6 +12,21 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -25,6 +40,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
